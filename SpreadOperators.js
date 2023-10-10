ede7619bf0fed3c5d49a70dbf338677ae4539ac2
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-10-10 17:58:43 +0300

    Initial Commit

---
+++ SpreadOperators.js
@@ -0,0 +1,192 @@
+//Array of Objects (5 Books)
+const data = [
+    {
+      id: 1,
+      title: "The Lord of the Rings",
+      publicationDate: "1954-07-29",
+      author: "J. R. R. Tolkien",
+      genres: [
+        "fantasy",
+        "high-fantasy",
+        "adventure",
+        "fiction",
+        "novels",
+        "literature",
+      ],
+      hasMovieAdaptation: true,
+      pages: 1216,
+      translations: {
+        spanish: "El señor de los anillos",
+        chinese: "魔戒",
+        french: "Le Seigneur des anneaux",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.52,
+          ratingsCount: 630994,
+          reviewsCount: 13417,
+        },
+        librarything: {
+          rating: 4.53,
+          ratingsCount: 47166,
+          reviewsCount: 452,
+        },
+      },
+    },
+    {
+      id: 2,
+      title: "The Cyberiad",
+      publicationDate: "1965-01-01",
+      author: "Stanislaw Lem",
+      genres: [
+        "science fiction",
+        "humor",
+        "speculative fiction",
+        "short stories",
+        "fantasy",
+      ],
+      hasMovieAdaptation: false,
+      pages: 295,
+      translations: {},
+      reviews: {
+        goodreads: {
+          rating: 4.16,
+          ratingsCount: 11663,
+          reviewsCount: 812,
+        },
+        librarything: {
+          rating: 4.13,
+          ratingsCount: 2434,
+          reviewsCount: 0,
+        },
+      },
+    },
+    {
+      id: 3,
+      title: "Dune",
+      publicationDate: "1965-01-01",
+      author: "Frank Herbert",
+      genres: ["science fiction", "novel", "adventure"],
+      hasMovieAdaptation: true,
+      pages: 658,
+      translations: {
+        spanish: "",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.25,
+          ratingsCount: 1142893,
+          reviewsCount: 49701,
+        },
+      },
+    },
+    {
+      id: 4,
+      title: "Harry Potter and the Philosopher's Stone",
+      publicationDate: "1997-06-26",
+      author: "J. K. Rowling",
+      genres: ["fantasy", "adventure"],
+      hasMovieAdaptation: true,
+      pages: 223,
+      translations: {
+        spanish: "Harry Potter y la piedra filosofal",
+        korean: "해리 포터와 마법사의 돌",
+        bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+        portuguese: "Harry Potter e a Pedra Filosofal",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.47,
+          ratingsCount: 8910059,
+          reviewsCount: 140625,
+        },
+        librarything: {
+          rating: 4.29,
+          ratingsCount: 120941,
+          reviewsCount: 1960,
+        },
+      },
+    },
+    {
+      id: 5,
+      title: "A Game of Thrones",
+      publicationDate: "1996-08-01",
+      author: "George R. R. Martin",
+      genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+      hasMovieAdaptation: true,
+      pages: 835,
+      translations: {
+        korean: "왕좌의 게임",
+        polish: "Gra o tron",
+        portuguese: "A Guerra dos Tronos",
+        spanish: "Juego de tronos",
+      },
+      reviews: {
+        goodreads: {
+          rating: 4.44,
+          ratingsCount: 2295233,
+          reviewsCount: 59058,
+        },
+        librarything: {
+          rating: 4.36,
+          ratingsCount: 38358,
+          reviewsCount: 1095,
+        },
+      },
+    },
+  ];
+  
+  
+  ///////////////////////////////////////////
+  
+  function getBooks() {
+    // console.log(data);
+  
+    return data;
+  }
+  ///////////////////////////////////////////
+  
+  function getBook(id) {
+  
+    console.log(data.find((d) => d.id === id));
+  
+    return data.find((d) => d.id === id);
+  }
+  
+
+  /* Rest Operators: Must be at the last element in the destructuring pattern  ...Arr = arr */
+ 
+  const book = getBook(3);
+  //book;
+  const {title, genres} = book;
+title;
+ const  [firstGen, ...allGen] =  genres;
+  firstGen;
+  allGen;
+
+
+
+  /* Spread Operators */
+
+  const newGen = [genres, 'epic fantasy'];
+  newGen;
+  // we need all  the elements one by one not in array shape ===> Spread Operator
+
+const SpreadGen = [...genres, 'Hidayah'];
+SpreadGen
+
+// Adding New Property
+const UpdatedBook = {book, moviePublicationDate: '10-Oct-2023'}
+UpdatedBook;
+
+// But this is not what we need {book, theNewProperty} ==> Sol: is to spread out the book properties
+
+const SecondUpdatedBook = {...book, moviePublicationDate: '07-Oct-2023'};
+SecondUpdatedBook;
+// Now everthing inside the SecondUpdatedBook Object
+
+
+// Updates Number of Pages (Existing Property):
+const ThirdUpdatedBook = {...book, moviePublicationDate:'0.7-Oct-2023', pages: 700};
+ThirdUpdatedBook;
+
